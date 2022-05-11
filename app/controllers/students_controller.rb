7e580eab728d9784b756d5428437e5ea48eb9dac
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2022-05-11 10:24:33 -0400

    Add routes and student view and actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,29 @@
 class StudentsController < ApplicationController
   def index
-    @student.find
+    @students = Student.all
   end
-  def create
-  end
+
   def new
+    @student = Student.new
   end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "Registration succeeded!"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def update
 
   end
-    
+
+  private
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :password, :email)
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
 class Student < ApplicationRecord
-  
+  has_secure_password
+
+  def full_name
+    "#{self.last_name} #{self.first_name}"
+  end
 end
